b556107eb4f3c3ed5f5e4f34571edb4abad451f9
Grogoiy Berezin <gregory_berezin@outlook.com>
2023-03-08 12:32:03 +0300

    add unsplash image

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
-import React from "react";
+import React, {useState} from "react"
 
 function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+  const slides = [
+    {
+      url: "https://images.unsplash.com/photo-1560343776-97e7d202ff0e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=435&q=80",
+    },
+    {
+      url: "https://images.unsplash.com/photo-1580164631075-b3f1304f4051?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+    },
+    {
+      url: "https://images.unsplash.com/photo-1521714161819-15534968fc5f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+    },
+    {
+      url: "https://images.unsplash.com/photo-1636840438199-9125cd03c3b0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=435&q=80",
+    },
+    {
+      url: "https://images.unsplash.com/photo-1608889345749-630640d94426?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80",
+    },
+  ]
+
+  return <div className="max-w-[1400px] h-[780px] m-auto py-16 px-4 relative">
+    
+  </div>
 }
 
-export default App;
+export default App
